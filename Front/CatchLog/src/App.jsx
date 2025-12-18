2d9611d15f8a9f3b6a307eca6e0f54419fa1c972
Gerry <gerrywiegand@gmail.com>
2025-12-18 14:02:27 -0500

    fixed routes, working

--- Front/CatchLog/src/App.jsx
+++ Front/CatchLog/src/App.jsx
@@ -1,17 +1,15 @@
 import { StrictMode } from "react";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./componenets/Home.jsx";
-import AddCatch from "./componenets/AddCatch.jsx";
+import Home from "./components/Home.jsx";
+import AddCatch from "./components/AddCatch.jsx";
 
 function App() {
-  
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add-catch" element={<AddCatch />} />
+        <Route path="/addcatch" element={<AddCatch />} />
       </Routes>
       <div>
         <h1>CatchLog App</h1>
