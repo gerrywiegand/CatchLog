aa630d78d96f68ece3b5363ac688a2592ab99edd
Gerry <gerrywiegand@gmail.com>
2025-12-18 23:25:00 -0500

    created catchCards for mobile. Home will render cards if mobile is detected over table

---
+++ Front/CatchLog/src/components/CatchCards.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import ScaleIcon from "@mui/icons-material/Scale";
+import StraightenIcon from "@mui/icons-material/Straighten";
+import PhishingIcon from "@mui/icons-material/Phishing";
+import Stack from "@mui/material/Stack";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import Chip from "@mui/material/Chip";
+
+export default function CatchCards({ catches, speciesMap }) {
+  return (
+    <div>
+      {catches.map((catchObj) => {
+        const speciesName = speciesMap[catchObj.species_id] || "Unknown";
+        return (
+          <Card key={catchObj.id}>
+            <CardContent>
+              <Stack
+                sx="width: 100%"
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <PhishingIcon fontSize="small" />
+                <Typography>{speciesName}</Typography>
+              </Stack>
+              <Stack
+                sx="width: 100%"
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <ScaleIcon fontSize="small" />
+                <Chip label={`${catchObj.weight} lbs`} Chip />
+              </Stack>
+              <Stack
+                sx="width: 100%"
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <StraightenIcon fontSize="small" />
+                <Chip label={`${catchObj.length} in`} Chip />
+              </Stack>
+              <Stack
+                sx="width: 100%"
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <AccessTimeIcon fontSize="small" />
+                <Typography variant="body2">
+                  {new Date(catchObj.date_caught).toLocaleDateString()}
+                </Typography>
+              </Stack>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
