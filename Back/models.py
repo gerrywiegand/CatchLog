a802b02ded4580d00674f1d857d889cb8e62276e
Gerry <gerrywiegand@gmail.com>
2025-12-20 12:32:59 -0500

    switching gears to auth

--- Back/models.py
+++ Back/models.py
@@ -73,6 +73,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     catches = db.relationship("Catch", back_populates="user")
 
